13c748b2894d20a08483f3412363f7068d8fc01f
17130018e <49342006+17130018e@users.noreply.github.com>
2019-05-07 17:14:59 +0500

    Update Test7.java

--- rent/src/test/java/com/group/rent/Test7.java
+++ rent/src/test/java/com/group/rent/Test7.java
@@ -18,6 +18,6 @@ public class Test7 {
 		/**
 		 * Сравнение результата и ожидаемого значения
 		 */
-		assertEquals(String.valueOf(a.getitog()), "20170.0");
+		assertEquals(String.valueOf(a.getitog()),"20170.0");
 	}
 }
